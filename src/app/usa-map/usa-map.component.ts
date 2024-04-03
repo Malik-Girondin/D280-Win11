659fe4981fb661a94a1bab209baa3dac6706834f
unknown <Malik Girondin>
2024-04-02 22:32:04 -0400

    Added map to the web page

---
+++ src/app/usa-map/usa-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usa-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './usa-map.component.html',
+  styleUrl: './usa-map.component.scss'
+})
+export class UsaMapComponent {
+
+}
