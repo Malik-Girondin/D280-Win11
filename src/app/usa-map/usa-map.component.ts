20cc4f5055818ed362c8a22d0ce551e1d4d01861
unknown <Malik Girondin>
2024-04-04 00:10:50 -0400

    Finish project, still does not work

--- src/app/usa-map/usa-map.component.ts
+++ src/app/usa-map/usa-map.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-usa-map',
@@ -8,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './usa-map.component.scss'
 })
 export class UsaMapComponent {
+  weather: any = {};
+
+  constructor (private apiService: ApiService) {}
+
+  setWeatherData(event: any) {
+    console.log('event', event.target.id);
+    this.apiService.fetchWeatherData(event.target.id).subscribe((data: any) => {
+      console.log(data)
+      this.weather = {
+        ...data,
+        state: event.target.getAttribute('title')
+      }
+    })
+  }
 
 }
